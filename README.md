a75f7c95eb55c8c881a7beb602f102a48249d879
Sean Son <40509013+SeanSon2005@users.noreply.github.com>
2022-03-31 13:58:54 -0700

    cleaned code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TroyFRC2022NeuralNet
+# code for client side deployment and roborio deployment here
--- imageprocessing.py
+++ imageprocessing.py
@@ -2,7 +2,17 @@ import cv2
 import numpy as np
 import time
 
-blue_ball = True
+blue_ball = True #determine ally color
+
+#PID controller coefficients
+Kp = 1 #coefficient for proportional
+Ki = 0 #coefficient for integral
+Kd = 0 #coefficient for derivative
+Kdist = 1 #coefficient for distance
+
+integral_previous = 0
+start_time = time.time()
+distance_avg = np.array([1,1,1,1,1,1])
 
 if(blue_ball):
   lower_threshold = np.array([100,40,40])
@@ -13,8 +23,6 @@ else:
   lower_threshold2 = np.array([170,40,40])
   upper_threshold2 = np.array([179,255,255])
 
-
-
 scale_factor = 1
 
 vid = cv2.VideoCapture(0)
@@ -23,17 +31,54 @@ ret, test_frame = vid.read()
 x_res = int(test_frame.shape[1]/scale_factor)
 y_res = int(test_frame.shape[0]/scale_factor)
 
+
+def distance_estimation(radius):
+    global distance_avg
+    distance_avg = np.append(np.delete(np.copy(distance_avg),0),radius)
+    tune = (int)(np.average(distance_avg)) - 50
+    if(tune < 0):
+        tune = 0
+    if(tune > 40):
+        tune = 40
+    return tune
+
+#PID calculations
+def PIDCalc(x_value, distance):
+  #declares integral previous and start time as the global ones
+  global integral_previous
+  global start_time
+
+  #x_value adjustments
+  x_value = 160 - x_value
+
+  #PID calculations
+  errorP = x_value * Kp
+  errorI = (integral_previous + (x_value * (time.time()-start_time))) * Ki
+  errorD = Kd
+  error = errorP + errorI + errorD
+  if (error > 0):
+      error += distance*Kdist
+  else:
+      error -= distance*Kdist
+
+  #updating integral
+  integral_previous = errorI
+
+  #update start time
+  start_time = time.time()
+  return error/120
+
+
 if(blue_ball):
   while True:
-    #success,frame = vidcap.read()
-    start_time = time.time()
-
+    #getting video frame
     ret, frame = vid.read()
-
-
     frame_scaled = cv2.resize(frame, dsize=(x_res, y_res), interpolation=cv2.INTER_CUBIC)
     
+    #convert image to HSV
     hsv = cv2.cvtColor(frame_scaled, cv2.COLOR_BGR2HSV)
+
+    #mask image with color range (blue)
     mask = cv2.inRange(hsv, lower_threshold, upper_threshold)
       
     #noise reduction code
@@ -48,25 +93,14 @@ if(blue_ball):
     if circles is not None:
       circles = np.uint16(np.around(circles))
       for i in circles[0,:]:
+        print("PID",PIDCalc(i[0],distance_estimation(i[2])))
+    
+    cv2.imshow("result",noise_reduction)
 
-        cv2.circle(frame_scaled,(i[0],i[1]),i[2],(0,255,0),2)
-        cv2.circle(frame_scaled,(i[0],i[1]),2,(0,0,255),3)
-
-    #cv2.imshow('circles', frame_scaled)
-    #cv2.imshow('what he sees',mask)
-
-    print("FPS: ", round(1.0 / (time.time() - start_time)))
-
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-          #break
 else:
   while True:
-    #success,frame = vidcap.read()
-    start_time = time.time()
 
     ret, frame = vid.read()
-
-
     frame_scaled = cv2.resize(frame, dsize=(x_res, y_res), interpolation=cv2.INTER_CUBIC)
     
     hsv = cv2.cvtColor(frame_scaled, cv2.COLOR_BGR2HSV)
@@ -79,25 +113,13 @@ else:
     mask_kernel = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     noise_reduction = cv2.blur(mask,(50,50))
     noise_reduction = cv2.inRange(noise_reduction,10,50)
-    noise_reduction = cv2.blur(noise_reduction,(15,15))
+    noise_reduction = cv2.blur(noise_reduction,(15,15)) 
 
     circles = cv2.HoughCircles(noise_reduction,cv2.HOUGH_GRADIENT,1.3,x_res,param1=50,param2=70,minRadius=1,maxRadius=120)
 
     if circles is not None:
       circles = np.uint16(np.around(circles))
       for i in circles[0,:]:
+        print("sadge")
 
-        cv2.circle(frame_scaled,(i[0],i[1]),i[2],(0,255,0),2)
-        cv2.circle(frame_scaled,(i[0],i[1]),2,(0,0,255),3)
-
-    cv2.imshow('circles', frame_scaled)
-    cv2.imshow('what he sees',mask)
-
-    print("FPS: ", round(1.0 / (time.time() - start_time)))
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
-
-
-vid.release()
-cv2.destroyAllWindows()
+    #print("FPS: ", round(1.0 / (time.time() - start_time)))
