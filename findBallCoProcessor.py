da8d4d054630f452b556cf2a8cec6366b1c9acc0
AC <addison@abcis.me>
2022-04-20 10:10:49 -0500

    gyugjglkjhkl

--- findBallCoProcessor.py
+++ findBallCoProcessor.py
@@ -49,19 +49,8 @@ if not testing_on_computer:
       print("Waiting")
       if not notified[0]:
           cond.wait()
-
+#get the table
 vision_nt = NetworkTables.getTable('Vision')
-
-#finds out team color through network tables
-blue_ball = vision_nt.getBoolean("blueBall")
-if(blue_ball):
-  lower_threshold = np.array([100,40,40])
-  upper_threshold = np.array([130,255,255])
-else:
-  lower_threshold = np.array([0,40,40])
-  upper_threshold = np.array([8,255,255])
-  lower_threshold2 = np.array([170,40,40])
-  upper_threshold2 = np.array([179,255,255])
 
 #PID calculations
 def PIDCalc(x_value):
@@ -86,8 +75,8 @@ def PIDCalc(x_value):
   return error/320
 
 
-if(blue_ball):
-  while True:
+
+while True:
     #starting time for finding Frames per second
     #start_time = time.time()
 
@@ -97,8 +86,23 @@ if(blue_ball):
     #convert image to HSV
     hsv = cv2.cvtColor(frame_scaled, cv2.COLOR_BGR2HSV)
 
-    #mask image with color range (blue)
-    mask = cv2.inRange(hsv, lower_threshold, upper_threshold)
+    #finds out team color through network tables
+    blue_ball = vision_nt.getBoolean("blueBall", True)
+
+    if(blue_ball):
+        #mask image with color range (blue)
+        lower_threshold = np.array([100,40,40])
+        upper_threshold = np.array([140,255,255])
+        mask = cv2.inRange(hsv, lower_threshold, upper_threshold)
+    else:
+        lower_threshold = np.array([0,40,40])
+        upper_threshold = np.array([8,255,255])
+        lower_threshold2 = np.array([170,40,40])
+        upper_threshold2 = np.array([179,255,255])
+        hsv = cv2.cvtColor(frame_scaled, cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(hsv, lower_threshold, upper_threshold)
+        mask2 = cv2.inRange(hsv, lower_threshold2, upper_threshold2)
+        mask = mask1 + mask2
       
     #noise reduction code (also reduimentary "contours")
     noise_reduction = cv2.blur(mask,(15,15))
@@ -117,37 +121,3 @@ if(blue_ball):
       vision_nt.putNumber('PID',PIDCalc(x_pos))
     else:
       vision_nt.putBoolean('seeBall',False)
-
-    
-    #Print Frames Per Second
-    #print("FPS: ", round(1.0 / (time.time() - start_time)))
-
-else:
-  while True:
-    #success,frame = vidcap.read()
-    start_time = time.time()
-
-    ret, frame = cvSink.grabFrame(img)
-    frame_scaled = cv2.resize(frame, dsize=(x_res, y_res), interpolation=cv2.INTER_CUBIC)
-    
-    hsv = cv2.cvtColor(frame_scaled, cv2.COLOR_BGR2HSV)
-    mask1 = cv2.inRange(hsv, lower_threshold, upper_threshold)
-    mask2 = cv2.inRange(hsv, lower_threshold2, upper_threshold2)
-    mask = mask1 + mask2
-      
-    #noise reduction code (also reduimentary "contours")
-    noise_reduction = cv2.blur(mask,(15,15))
-    noise_reduction = cv2.inRange(noise_reduction,1,70)
-    noise_reduction = cv2.blur(noise_reduction,(15,15))
-
-    #find Circles
-    circles = cv2.HoughCircles(noise_reduction,cv2.HOUGH_GRADIENT,1.3,minDist=25,param1=50,param2=70,minRadius=10,maxRadius=120)
-
-    if circles is not None:
-      circles = np.uint16(np.around(circles))
-      max_radius = math.floor((circles.argmax()+1)/3)
-      x_pos = circles[0,max_radius,0]
-      #print(PIDCalc(x_pos))
-      vision_nt.putNumber('PID',PIDCalc(x_pos))
-
-    #print("FPS: ", round(1.0 / (time.time() - start_time)))
