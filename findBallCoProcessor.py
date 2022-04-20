73e872a6fb7fed831817dd71981bdd74d375fe39
Sean Son <40509013+SeanSon2005@users.noreply.github.com>
2022-04-20 08:55:23 -0700

    added Camera Server

--- findBallCoProcessor.py
+++ findBallCoProcessor.py
@@ -13,20 +13,29 @@ Kp = 1 #coefficient for proportional
 Ki = 0.2 #coefficient for integral
 Kd = 0 #coefficient for derivative
 
+x_res = 640 #camera width
+y_res = 360 #camera height
+
 integral_previous = 0
 start_time = time.time()
 
 scale_factor = 1
 
 cs = CameraServer.getInstance()
+cs.setResolution(x_res, y_res)
 cs.enableLogging()
 
 camera = cs.startAutomaticCapture()
 
 cvSink = cs.getVideo()
+output = cs.putVideo("Front Camera", x_res, y_res)
 
-x_res = 640
-y_res = 360
+lower_thresholdRED = np.array([0,40,40])
+upper_thresholdRED = np.array([8,255,255])
+lower_threshold2RED = np.array([170,40,40])
+upper_threshold2RED = np.array([179,255,255])
+lower_threshold = np.array([100,40,40])
+upper_threshold = np.array([140,255,255])
 
 img = np.zeros(shape=(x_res, y_res, 3), dtype=np.uint8)
 
@@ -91,17 +100,12 @@ while True:
 
     if(blue_ball):
         #mask image with color range (blue)
-        lower_threshold = np.array([100,40,40])
-        upper_threshold = np.array([140,255,255])
         mask = cv2.inRange(hsv, lower_threshold, upper_threshold)
     else:
-        lower_threshold = np.array([0,40,40])
-        upper_threshold = np.array([8,255,255])
-        lower_threshold2 = np.array([170,40,40])
-        upper_threshold2 = np.array([179,255,255])
+        #mask image with color range (red)
         hsv = cv2.cvtColor(frame_scaled, cv2.COLOR_BGR2HSV)
-        mask1 = cv2.inRange(hsv, lower_threshold, upper_threshold)
-        mask2 = cv2.inRange(hsv, lower_threshold2, upper_threshold2)
+        mask1 = cv2.inRange(hsv, lower_thresholdRED, upper_thresholdRED)
+        mask2 = cv2.inRange(hsv, lower_threshold2RED, upper_threshold2RED)
         mask = mask1 + mask2
       
     #noise reduction code (also reduimentary "contours")
@@ -110,6 +114,10 @@ while True:
     noise_reduction = cv2.blur(noise_reduction,(15,15))
 
     #find Circles
+    #param1 canny edge parameter
+    #param2 the strictness of circle detection
+    #minRadius the minimum radius for a circle detection
+    #maxRadius the maximum radius for a circle detection
     circles = cv2.HoughCircles(noise_reduction,cv2.HOUGH_GRADIENT,1.3,minDist=25,param1=50,param2=70,minRadius=10,maxRadius=120)
 
     if circles is not None:
@@ -121,3 +129,5 @@ while True:
       vision_nt.putNumber('PID',PIDCalc(x_pos))
     else:
       vision_nt.putBoolean('seeBall',False)
+
+    output.putFrame(frame_scaled) #puts frame into Camera Server
